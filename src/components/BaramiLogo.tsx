6514704cb152ce0904e2f15c986fef0244556cbd
park-byeong-gwan <impbk2002@gmail.com>
2023-09-13 10:59:04 +0900

    fix logo svg line spacing

--- src/components/BaramiLogo.tsx
+++ src/components/BaramiLogo.tsx
@@ -2,7 +2,7 @@ import { ReactComponent as BaramiSVG } from "../icon/barami.svg"
 import React from "react"
 
 const BaramiLogo: React.FC<React.SVGProps<SVGSVGElement>> = (props) => {
-    return <BaramiSVG title="barami" {...props} />
+    return <BaramiSVG {...props} />
 }
 
 export default BaramiLogo
